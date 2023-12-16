3de1bd76f5ff6098f36413a0ce8a2500f9937a2c
Aman singh <98640922+Amansingh1210@users.noreply.github.com>
2023-12-16 18:06:50 +0000

    Add all the products from data folder

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,43 +1,5 @@
-const products = [
-    {
-        image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-        name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-        rating: {
-            stars: 4.5,
-            count: 87
-        },
-        pricerupees: 905
-    },
-    {
-        image: "images/products/intermediate-composite-basketball.jpg",
-        name: "Intermediate Size Basketball",
-        rating: {
-            stars: 4,
-            count: 127
-        },
-        pricerupees: 1400
-    },
-    {
-        image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-        name: "Adults Plain Cotton T-Shirt - 2 Pack",
-        rating: {
-            stars: 4.5,
-            count: 56
-        },
-        pricerupees: 660
-    },
-    {
-        image: "images/products/black-2-slot-toaster.jpg",
-        name: "2 Slot Toaster - Black",
-        rating: {
-            stars: 5,
-            count: 2197
-        },
-        pricerupees: 1500
-    },
-]
- 
-let productsHTML = ""
+ let productsHTML = ""
+
 
 products.forEach((product) => {
     productsHTML  += `   
@@ -56,7 +18,7 @@ products.forEach((product) => {
             </div>
 
             <div class="product-price">
-             Rs.${(product.pricerupees).toFixed(2)}
+             Rs.${(product.priceCents).toFixed(2)}
             </div>
 
             <div class="product-quantity-container">
