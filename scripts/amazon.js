42c0f5b38d22baff246ce6b42ae8780f024bf954
Aman singh <98640922+Amansingh1210@users.noreply.github.com>
2023-12-16 18:41:58 +0000

    Add the " add to cart " functionality to our project

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -43,7 +43,8 @@ products.forEach((product) => {
             Added
             </div>
 
-            <button class="add-to-cart-button button-primary">
+            <button class="add-to-cart-button button-primary js-add-to-cart"
+            data-product-id="${product.id}">
             Add to Cart
             </button>
             </div>
@@ -51,6 +52,38 @@ products.forEach((product) => {
 
 });
 
-console.log(productsHTML);
-
 document.querySelector(".js-products-grid").innerHTML = productsHTML
+
+// Add the products to card 
+
+document.querySelectorAll(".js-add-to-cart").forEach((button)=>{
+    button.addEventListener('click',() => {
+        const productId = button.dataset.productId;
+
+        let matchingItem ; 
+
+        cart.forEach((item)=>{
+            if (productId==item.productId){
+                matchingItem = item ;
+            }
+        });
+
+        if(matchingItem){
+            matchingItem.quantity += 1;
+        }
+        else {
+        cart.push({
+            productId: productId,
+            quantity: 1 
+        })
+        }
+
+        let cartQuantity = 0 ;
+        cart.forEach((item) => {
+            cartQuantity += item.quantity
+        })
+
+        document.querySelector(".js-cart-quantity").innerHTML = cartQuantity
+
+    }) 
+});
