4aced18e431654229d822114c5f8058f8e253379
Aman singh <98640922+Amansingh1210@users.noreply.github.com>
2023-12-17 08:16:18 +0000

    create "deleteProduct" function to remove items from cart

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [
+export let cart = [
     {
         productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
         quantity: 2,
@@ -9,7 +9,7 @@ export const cart = [
     }
 ];
 
-
+// FUNCTION TO ADD PRODUCT TO CARAT 
 export function addToCart(productId){
       
     let matchingItem ; 
@@ -33,7 +33,7 @@ export function addToCart(productId){
     })
     }
 }
-
+// FUNCTION TO UPDATE CART QUANTITY
 export function updateCart(){
     let cartQuantity = 0 ;
     cart.forEach((cartItem) => {
@@ -42,3 +42,15 @@ export function updateCart(){
 
     document.querySelector(".js-cart-quantity").innerHTML = cartQuantity
 }
+// FUNCTION TO DELETE PRODUCT FROM CART
+export function deleteItem(productId){
+    const newCart = [];
+
+    cart.forEach((cartItem)=>{
+        if(cartItem.productId !== productId){
+            newCart.push(cartItem);
+        }
+    });
+    cart = newCart;
+}
+
