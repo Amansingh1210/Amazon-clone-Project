810227ce2ebae766f246a5602cddf568bb6b284f
Aman singh <98640922+Amansingh1210@users.noreply.github.com>
2023-12-17 04:33:35 +0000

    Create the modules  and import cart and products

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,6 @@
+ import { cart } from "../data/cart.js";
+ import { products } from "../data/products.js";
+
  let productsHTML = ""
 
 
@@ -22,7 +25,7 @@ products.forEach((product) => {
             </div>
 
             <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
                 <option selected value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -60,21 +63,25 @@ document.querySelectorAll(".js-add-to-cart").forEach((button)=>{
     button.addEventListener('click',() => {
         const productId = button.dataset.productId;
 
+        
         let matchingItem ; 
-
+        
         cart.forEach((item)=>{
             if (productId==item.productId){
                 matchingItem = item ;
             }
         });
 
+        const quantitySelector =  document.querySelector(`.js-quantity-selector-${productId}`);
+        const quantity = Number.parseInt(quantitySelector.value)
+
         if(matchingItem){
-            matchingItem.quantity += 1;
+            matchingItem.quantity += quantity;
         }
         else {
         cart.push({
             productId: productId,
-            quantity: 1 
+            quantity: quantity
         })
         }
 
